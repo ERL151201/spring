31278984b30bf02d374d475829958822bcf2516e
Emanuel Rangel <emanuelrangelluna@gmail.com>
2021-03-08 12:28:39 -0600

    Hola mundo

---
+++ src/main/java/com/emanuel/market/EmanuelMarketApplication.java
@@ -0,0 +1,13 @@
+package com.emanuel.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmanuelMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmanuelMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emanuel/market/EmanuelMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanuel.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmanuelMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
