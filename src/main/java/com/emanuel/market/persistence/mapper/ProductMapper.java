a59c11b2ce0aecb28ab94d7e1afad9c6c51327b6
Emanuel Rangel <emanuelrangelluna@gmail.com>
2021-03-22 18:20:16 -0600

    4_3 dominio mapscrupt

---
+++ src/main/java/com/emanuel/market/persistence/mapper/ProductMapper.java
@@ -0,0 +1,30 @@
+package com.emanuel.market.persistence.mapper;
+
+import com.emanuel.market.domain.Product;
+import com.emanuel.market.persistence.entity.Producto;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class})
+public interface ProductMapper {
+    @Mappings({
+            @Mapping(source = "idProducto", target = "productId"),
+            @Mapping(source = "nombre", target = "name"),
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "pecioVenta", target = "price"),
+            @Mapping(source = "cantidadStock", target = "stock"),
+            @Mapping(source = "estado", target = "active"),
+            @Mapping(source = "categoria", target = "category"),
+
+    })
+    Product toProduc(Producto producto);
+    List<Product> toProducts(List<Producto> productos);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "codigoBarras", ignore = true)
+    Producto toProducto(Product product);
+}
