a90ff93683a626fc4f7cc823e584350abea27693
Emanuel Rangel <emanuelrangelluna@gmail.com>
2021-03-24 13:27:47 -0600

    5_2 Crear dominio compras

---
+++ src/main/java/com/emanuel/market/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.emanuel.market.domain.repository;
+
+import com.emanuel.market.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
